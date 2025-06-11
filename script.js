8a9a4267b974c0a7edaa97f8e4b87a13540d4d4c
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-06-11 17:13:34 +0200

    push

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+/* Dato un array di oggetti rappresentante un team di un’azienda, creare una pagina dedicata in cui mostrare una card per ciascun componente.
+(trovate l’array del team all’interno della cartella in allegato)
+Bonus
+Rendere l’esercizio responsive, mandando a capo le card
+Aggiungere un form di agginta membri che permetta di visualizzare il nuovo membro sulla pagina
+(usate una foto qualunque, anche vostra se volete sentirvi parte del team! */
+
+
+
+//  array che rappresenta un team di un azienda
+
 const teamMembers = [
   {
     name: "Marco Bianchi",
@@ -36,3 +47,59 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+console.log(teamMembers);
+
+
+const rowEl = document.querySelector(".row");
+
+
+let cards = "";
+
+// Ciclo for per ogni membro 
+for (let i = 0; i < teamMembers.length; i++) {
+  const member = teamMembers[i];
+  cards +=
+
+
+    // generazione del template literal ``
+    `
+    
+  <div class="card mb-3">
+   <div class="row g-4">
+     <div class="col-md-4">
+       <img src="${member.img}">
+     </div>
+     <div class="col-md-8">
+       <div class="card-body">
+         <h5 class="card-title">${member.name}</h5>
+         <p class="card-text">${member.role}</p>
+         <p class="card-text">${member.email}</p>
+      </div >
+    </div >
+  </div >
+</div> 
+
+ `
+}
+
+// Inserisci tutte le card nel container
+rowEl.innerHTML = cards;
+
+
+
+
+const addTeam = [
+  {
+    name: "Umberto Carbone",
+    role: "Developer",
+    email: "umbertocarbone@team.com",
+    img: "img/umbcar.jpg"
+  }
+
+]
+  const nuovoTeam = [...teamMembers, ...addTeam]
+console.log(rowEl)
+
+
+
